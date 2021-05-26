654bef3371c007481ff42af623d70acdcf063d4d
André <andre_farao123@hotmail.com>
2021-05-25 23:23:05 -0300

    Add react router dom and initial routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Dashboard } from 'pages/Dashboard';
+import { Repository } from 'pages/Repository';
+import { Route, Switch } from 'react-router-dom';
+
+export const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
