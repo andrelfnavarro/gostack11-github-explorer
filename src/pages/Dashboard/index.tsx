c0785cb0e0fa654ae5b4075bf4fd3a0270635636
André <andre_farao123@hotmail.com>
2021-05-26 22:03:44 -0300

    Fetcihng repos on github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,53 +1,63 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import logoImg from 'assets/logo.svg';
 import { FiChevronRight } from 'react-icons/fi';
+import api from 'services/api';
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  // eslint-disable-next-line camelcase
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    // eslint-disable-next-line camelcase
+    avatar_url: string;
+  };
+}
+
 export const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Array<Repository>>([]);
+
+  const addRepository = async (
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    const repository = response.data;
+
+    setRepositories((prevRepositories) => [...prevRepositories, repository]);
+    setNewRepo('');
+  };
+
   return (
     <>
-      <img src={logoImg} alt="Logo" />
+      <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={addRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            alt="André Navarro"
-            src="https://avatars.githubusercontent.com/u/32081314?v=4"
-          />
-          <div>
-            <strong>andrelfnavarro/elixir_training</strong>
-            <p>The Pragmatic Studio Elixir course</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            alt="André Navarro"
-            src="https://avatars.githubusercontent.com/u/32081314?v=4"
-          />
-          <div>
-            <strong>andrelfnavarro/elixir_training</strong>
-            <p>The Pragmatic Studio Elixir course</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            alt="André Navarro"
-            src="https://avatars.githubusercontent.com/u/32081314?v=4"
-          />
-          <div>
-            <strong>andrelfnavarro/elixir_training</strong>
-            <p>The Pragmatic Studio Elixir course</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              alt={repository.owner.login}
+              src={repository.owner.avatar_url}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
