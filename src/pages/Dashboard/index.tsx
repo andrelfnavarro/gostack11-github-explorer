284e6ec949e0f67809ff9f700a40990a7aa1b5d2
André <andre_farao123@hotmail.com>
2021-05-26 21:18:06 -0300

    Style dashboard and add logos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,54 @@
 import React from 'react';
+import logoImg from 'assets/logo.svg';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
 
 export const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Logo" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            alt="André Navarro"
+            src="https://avatars.githubusercontent.com/u/32081314?v=4"
+          />
+          <div>
+            <strong>andrelfnavarro/elixir_training</strong>
+            <p>The Pragmatic Studio Elixir course</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            alt="André Navarro"
+            src="https://avatars.githubusercontent.com/u/32081314?v=4"
+          />
+          <div>
+            <strong>andrelfnavarro/elixir_training</strong>
+            <p>The Pragmatic Studio Elixir course</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            alt="André Navarro"
+            src="https://avatars.githubusercontent.com/u/32081314?v=4"
+          />
+          <div>
+            <strong>andrelfnavarro/elixir_training</strong>
+            <p>The Pragmatic Studio Elixir course</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
