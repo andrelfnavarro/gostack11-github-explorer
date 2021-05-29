34f457d3383396bf8b82f93fbf11444248cc6f36
André Navarro <andre_farao123@hotmail.com>
2021-05-28 21:09:47 -0300

    Handling errors on search field

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, FormEvent } from 'react';
 import logoImg from 'assets/logo.svg';
 import { FiChevronRight } from 'react-icons/fi';
 import api from 'services/api';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
   // eslint-disable-next-line camelcase
@@ -17,17 +17,29 @@ interface Repository {
 
 export const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Array<Repository>>([]);
 
   const addRepository = async (
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> => {
     event.preventDefault();
-    const response = await api.get<Repository>(`repos/${newRepo}`);
-    const repository = response.data;
 
-    setRepositories((prevRepositories) => [...prevRepositories, repository]);
-    setNewRepo('');
+    if (!newRepo) {
+      setInputError('Digite autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+      const repository = response.data;
+
+      setRepositories((prevRepositories) => [...prevRepositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório');
+    }
   };
 
   return (
@@ -35,7 +47,7 @@ export const Dashboard: React.FC = () => {
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form onSubmit={addRepository}>
+      <Form hasError={!!inputError} onSubmit={addRepository}>
         <input
           value={newRepo}
           onChange={(e) => setNewRepo(e.target.value)}
@@ -43,6 +55,8 @@ export const Dashboard: React.FC = () => {
         />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      {inputError && <Error>{inputError}</Error>}
 
       <Repositories>
         {repositories.map((repository) => (
